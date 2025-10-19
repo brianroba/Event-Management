609235e5257f3b8d4d0039e6e72082924bd9a246
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-10-19 20:56:58 +0300

    Initial deploy

--- event_manager/event_manager/settings.py
+++ event_manager/event_manager/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
